d230c0eb4728c5b236a42809c71ac82c1d94dd41
Touheed Sab <touheed463@gmail.com>
2024-03-30 02:16:55 +0530

    modified the file for mobile responsive.

--- examfront/src/app/pages/admin/sidebar/sidebar.component.ts
+++ examfront/src/app/pages/admin/sidebar/sidebar.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
-import { BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  constructor(private loginService:LoginService,private router:Router,public breakpointObserver: BreakpointObserver){}
-
-  isMenuOpen = true;
+  constructor(private loginService:LoginService,private router:Router){}
 
   ngOnInit(){
     
@@ -21,15 +18,6 @@ export class SidebarComponent {
     this.loginService.logOut();
     this.loginService.loginStatusSubject.next(true);
     this.router.navigate(["/login"])
-  }
-
- 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-
-  isSmallScreen() {
-    return this.breakpointObserver.isMatched('(max-width: 900px)');
   }
 
 }
